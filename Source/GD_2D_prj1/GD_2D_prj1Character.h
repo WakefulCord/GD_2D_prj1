3320bd44c5ada3d4d0e4ebb38b222d1e0c5eab06
Tom Cole <tom.cole04@outlook.com>
2025-02-27 22:28:45 +0000

    Revert "Merge branch 'main' of https://github.com/WakefulCord/GD_2D_prj1"
    This reverts commit f57381590143b282f341e5538de6efb758232ef5, reversing
    changes made to 68bfc65617339cc2018cb655f4a98dc98ba5ad88.

--- Source/GD_2D_prj1/GD_2D_prj1Character.h
+++ Source/GD_2D_prj1/GD_2D_prj1Character.h
@@ -4,8 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
-#include "EnhancedInputComponent.h"
-#include "InputActionValue.h"
 #include "GD_2D_prj1Character.generated.h"
 
 class UTextRenderComponent;
@@ -18,17 +16,10 @@ class UTextRenderComponent;
  * The CharacterMovementComponent (inherited from ACharacter) handles movement of the collision capsule
  * The Sprite component (inherited from APaperCharacter) handles the visuals
  */
-
-
 UCLASS(config=Game)
-class UInputMappingContext;
 class AGD_2D_prj1Character : public APaperCharacter
 {
 	GENERATED_BODY()
-
-	// The Players input mapping context
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "EnhancedInput")
-	UInputMappingContext* InputMapping;
 
 	/** Side view camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
