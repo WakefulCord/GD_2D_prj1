3ea2ae008bea43c2a224b55a7e1298e1796ea43f
Tom Cole <tom.cole04@outlook.com>
2025-03-17 11:06:54 +0000

    Week 8 Start
    Lack of previous work stopping me from continuing. Need to catch up on previous labs work to get the rest done by next week

---
+++ Source/GD_2D_prj1/Public/CharacterState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CharacterState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GD_2D_PRJ1_API UCharacterState : public UObject
+{
+	GENERATED_BODY()
+	
+};
